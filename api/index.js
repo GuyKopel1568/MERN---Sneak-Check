18e49662b25294a73952fd59d21969a1a1589dd0
Guy Kopel <guykopel1568@gmail.com>
2024-02-19 09:12:13 +0200

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config({ path: 'config.env' });
+
+console.log(process.env.DATABASE_URL);
+console.log(process.env.DATABASE_PASS);
+
+//ENTER THE DB PASSWORD
+const DB = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASS,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successfully!!!'))
+  .catch((error) => console.error('DB connection error:', error));
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
