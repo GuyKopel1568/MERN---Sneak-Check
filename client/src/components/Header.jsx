ceb59731b23a674f03d7e0afd4206da104d1f97e
Guy Kopel <guykopel1568@gmail.com>
2024-02-21 13:40:04 +0200

    Create collapse Navbar, responsive to window size

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,45 +1,97 @@
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className=" navbar_container p-2 bg-transparent shadow-md rounded-3xl mx-auto">
+    <header className="navbar_container p-2 bg-transparent shadow-md rounded-3xl mx-auto relative">
       <div className="flex items-center justify-between max-auto max-w-6xl">
-        <Link to="/">
-          <h1
-            className="font-bold text-white sm:text-lg flex flex-wrap"
-            style={{ fontSize: '2rem' }}
-          >
-            <span className="text-[#a3a3a3]">Sneak</span>
-            <span className="text-[#525252]">Check</span>
-          </h1>
-        </Link>
-        <form className="ml-10 p-2 bg-white bg-opacity-30 rounded-xl flex">
-          <input
-            type="text"
-            placeholder="Search..."
-            className=" bg-transparent focus:outline-none  sm:w-80"
-          />
-          <FaSearch className="text-white" />
-        </form>
-        <ul className="flex text-white font-semibold text-lg gap-4">
+        <div className="flex items-center">
           <Link to="/">
-            <li className=" navlist hidden sm:inline hover:underline  transition duration-300">
-              Home
-            </li>
+            <h1 className="font-bold text-white sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+              <span className="text-[#a3a3a3]">Sneak</span>
+              <span className="text-[#525252]">Check</span>
+            </h1>
           </Link>
-          <Link to="/about">
-            <li className="navlist hover:underline transition duration-300">
-              About
-            </li>
+        </div>
+        <div className="flex items-center">
+          <form className="flex p-2 bg-white bg-opacity-30 rounded-xl items-center">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-transparent focus:outline-none lg:w-96 sm:w-64"
+            />
+            <button type="submit" className="focus:outline-none">
+              <FaSearch className="text-white" />
+            </button>
+          </form>
+        </div>
+        <div className="hidden md:flex items-center space-x-4">
+          <Link
+            to="/"
+            className="font-semibold text-lg text-white hover:underline transition duration-300"
+          >
+            Home
           </Link>
-          <Link to="/sign-in">
-            <li className="navlist hover:underline  transition duration-300">
-              Sign-in
-            </li>
+          <Link
+            to="/about"
+            className="font-semibold text-lg text-white hover:underline transition duration-300"
+          >
+            About
           </Link>
-        </ul>
+          <Link
+            to="/sign-in"
+            className="font-semibold text-lg text-white hover:underline transition duration-300"
+          >
+            Sign-in
+          </Link>
+        </div>
+
+        <div className="md:hidden ml-4">
+          {isOpen ? (
+            <FaTimes
+              className="text-white text-2xl cursor-pointer"
+              onClick={toggleNavbar}
+            />
+          ) : (
+            <FaBars
+              className="text-white text-2xl cursor-pointer"
+              onClick={toggleNavbar}
+            />
+          )}
+        </div>
       </div>
+
+      {isOpen && (
+        <div className="nav_collapse md:hidden absolute inset-x-0  bg-white p-2 shadow-md rounded-3xl">
+          <ul className="flex flex-col items-center font-semibold text-lg gap-4">
+            <Link
+              to="/"
+              className="navlist hover:underline transition duration-300"
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="navlist hover:underline transition duration-300"
+            >
+              About
+            </Link>
+            <Link
+              to="/sign-in"
+              className="navlist hover:underline transition duration-300"
+            >
+              Sign-in
+            </Link>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
